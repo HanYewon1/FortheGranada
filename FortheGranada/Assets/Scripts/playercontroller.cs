c86f8e3f4b9d24c1f7e584ed6e7e66023969a2ff
Yewon Han <cherry01614@naver.com>
2024-11-16 16:02:34 +0900

    플레이어 이동 복원 -예원

--- FortheGranada/Assets/Scripts/playercontroller.cs
+++ FortheGranada/Assets/Scripts/playercontroller.cs
@@ -7,6 +7,8 @@ public class playercontroller : MonoBehaviour
     public float player_speed;//플레이어 이동속도
 
     Rigidbody2D rigidbody2d;
+    Animator animator;
+
     float player_x;//좌우 움직임
     float player_y;//상하 움직임
 
@@ -14,9 +16,22 @@ public class playercontroller : MonoBehaviour
 
     private void Awake()
     {
+        animator = GetComponent<Animator>();
         rigidbody2d = GetComponent<Rigidbody2D>();
     }
     void Update()
+    {
+        PlayerMove();   
+
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 move_vec = is_horizon_move ? new Vector2(player_x, 0) : new Vector2(0, player_y);
+        rigidbody2d.linearVelocity = move_vec * player_speed;
+    }
+
+    private void PlayerMove()
     {
         player_x = Input.GetAxisRaw("Horizontal"); //좌우 이동
         player_y = Input.GetAxisRaw("Vertical"); //상하 이동
@@ -28,18 +43,27 @@ public class playercontroller : MonoBehaviour
         bool player_y_up = Input.GetButtonUp("Vertical");
 
         //상하좌우 이동을 위한 조건
-        if (player_x_down || player_y_up)
+        if (player_x_down)
         {
             is_horizon_move = true; //좌우 이동
         }
-        else if (player_y_down || player_x_up)
-        {
+        else if (player_y_down)
             is_horizon_move = false; //상하 이동
+        else if (player_x_up || player_y_up)
+            is_horizon_move = player_x != 0;
+
+        //애니메이션
+        if (animator.GetInteger("player_move_x") != player_x) //좌우
+        {
+            animator.SetBool("is_change", true);
+            animator.SetInteger("player_move_x", (int)player_x);
         }
-    }
-    private void FixedUpdate()
-    {
-        Vector2 move_vec = is_horizon_move ? new Vector2(player_x, 0) : new Vector2(0, player_y);
-        rigidbody2d.linearVelocity = move_vec * player_speed;
+        else if (animator.GetInteger("player_move_y") != player_y)
+        {
+            animator.SetBool("is_change", true);
+            animator.SetInteger("player_move_y", (int)player_y);
+        }//상하
+        else //idle
+                    animator.SetBool("is_change", false);
     }
 }
