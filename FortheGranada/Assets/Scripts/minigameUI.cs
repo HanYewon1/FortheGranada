b7655a6c0de6b2d174fe8923320549ddb8c7e959
JiHun <pgh979@gmail.com>
2024-11-25 03:20:59 +0900

    Add Images

--- FortheGranada/Assets/Scripts/minigameUI.cs
+++ FortheGranada/Assets/Scripts/minigameUI.cs
@@ -1,4 +1,4 @@
-    using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -44,7 +44,7 @@ public class minigameUI : MonoBehaviour
                 {
                     if (txt_list[j].text != "NULL")
                     {
-                        Debug.Log("!NULL");
+                        //Debug.Log("!NULL");
                     }
                     else if (j == LLM)
                     {
