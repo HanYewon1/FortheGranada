b98de1e93a230f373de07f0802ac0b82335f8a52
Yewon Han <cherry01614@naver.com>
2024-11-30 18:02:53 +0900

    npc chase 구현중

--- FortheGranada/Assets/Scripts/npcchase.cs
+++ FortheGranada/Assets/Scripts/npcchase.cs
@@ -8,30 +8,33 @@ public class npcchase : MonoBehaviour
 
     public float stopChase;
 
-    private Transform target;
-    private int[,] gridMap = {
-        { 0, 0, 0, 1, 0 },
-        { 0, 1, 0, 1, 0 },
-        { 0, 1, 0, 0, 0 },
-        { 0, 0, 0, 1, 0 },
-        { 0, 1, 0, 0, 0 }
-    };
-
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         npc_sight = GetComponent<npcsight>();
         npc_controller = GetComponent<npccontroller>();
-        target = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Chase();
-    }
-
-    void Chase()
-    {
+        if (npc_sight.DetectPlayer == true && !npc_controller.isChasing)
+        {
+            Debug.Log("Start chasing player.");
+            npc_controller.isChasing = true;
+            npc_controller.StartChasing(); // 추격 시작
+            // 추격 로직 (DFS 등)
+        }
+        else if (!npc_sight.DetectPlayer && npc_controller.isChasing)
+        {
+            if (npc_sight.Target == null || Vector2.Distance(transform.position, npc_sight.Target.position) > stopChase)
+            {
+                Debug.Log("Stop chasing player. Returning to patrol.");
+                npc_controller.isChasing = false;
+                npc_controller.StopChasing(); // 추격 중단 후 순찰 복귀
+            }
+        }
     }
 }
+
+  
