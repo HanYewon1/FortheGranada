dff924ac4758afdf156b595f2f8568f680165a5d
Yewon Han <cherry01614@naver.com>
2024-12-04 03:43:10 +0900

    Revert "NPC 수정"
    This reverts commit 6afcca6bc1ba60ae0d1c4705732be54cf16e472e.

--- FortheGranada/Assets/Scripts/npcattack.cs
+++ FortheGranada/Assets/Scripts/npcattack.cs
@@ -1,3 +1,4 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class npcattack : MonoBehaviour
@@ -14,6 +15,7 @@ public class npcattack : MonoBehaviour
     void Start()
     {
         npc_sight = GetComponent<npcsight>();
+        weaponPrefab = Resources.Load<GameObject>("weapon");
         if (weaponPrefab == null)
         {
             Debug.LogError("Weapon prefab could not be loaded from Resources folder. Check the path and filename.");
