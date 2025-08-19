7082ecb8d788970e41dd15a8c0b8ae79b032f7e0
Yewon Han <cherry01614@naver.com>
2025-08-19 10:22:36 +0900

    Update npcsight.cs
    주석 수정

--- FortheGranada/Assets/Scripts/npcsight.cs
+++ FortheGranada/Assets/Scripts/npcsight.cs
@@ -21,6 +21,7 @@ public class npcsight : MonoBehaviour
 
     void Start()
     {
+        //vieMeshFilter 연결 안되어있을 때 자식 객체에서 가져옴
         npc_controller = GetComponent<npccontroller>();
         if (viewMeshFilter == null)
             viewMeshFilter = GetComponentInChildren<MeshFilter>();
@@ -37,21 +38,22 @@ public class npcsight : MonoBehaviour
         DrawFieldOfView();
     }
 
-    private void Detect()
+    private void Detect() //플레이어 탐지
     {
         Collider2D[] rangeCheck = Physics2D.OverlapCircleAll(transform.position, radius, targetLayer);
 
         if (rangeCheck.Length > 0)
         {
+            //첫번째로 감지된 타겟
             Transform target = rangeCheck[0].transform;
             Vector3 directionToTarget = (target.position - transform.position).normalized;
 
-            // �þ� ���� ���ο� �ִ��� Ȯ��
+            // NPC가 바라보는 방향과 플레이어 방향 각도가 시야 각도 이내인지
             if (Vector3.Angle(new Vector3(npc_controller.movement.x, npc_controller.movement.y, 0), directionToTarget) < angle / 2)
             {
                 float distanceToTarget = Vector3.Distance(transform.position, target.position);
 
-                // ��ֹ��� ���� �������� �ʾҴ��� Ȯ��
+                // NPC와 플레이어 사이에 장애물 없으면 탐지 성공
                 if (!Physics2D.Raycast(transform.position, directionToTarget, distanceToTarget, obstructionLayer))
                 {
                     DetectPlayer = true;
@@ -66,21 +68,21 @@ public class npcsight : MonoBehaviour
         Target = null;
 
       }
-    private void DrawFieldOfView()
+    private void DrawFieldOfView() //시야메쉬 그리기
     {
         Vector3 forwardDirection = new Vector3(npc_controller.movement.x, npc_controller.movement.y, 0);
         Vector3[] vertices = new Vector3[segments + 2];
         int[] triangles = new int[segments * 3];
 
-        vertices[0] = Vector3.zero; // �߽���
+        vertices[0] = Vector3.zero; // 메쉬 중심
 
-        for (int i = 0; i <= segments; i++)
+        for (int i = 0; i <= segments; i++) //시야 각도를 기준으로 분할해서 메쉬 꼭짓점 계산
         {
             float currentAngle = -angle / 2 + (angle / segments) * i;
             Vector3 direction = RotateVector(forwardDirection, currentAngle).normalized;
             vertices[i + 1] = direction * radius;
 
-            if (i < segments)
+            if (i < segments) //메쉬 그릴 인덱스 지정
             {
                 triangles[i * 3] = 0;
                 triangles[i * 3 + 1] = i + 1;
@@ -88,6 +90,7 @@ public class npcsight : MonoBehaviour
             }
         }
 
+        //메쉬 갱신
         viewMesh.Clear();
         viewMesh.vertices = vertices;
         viewMesh.triangles = triangles;
@@ -95,7 +98,7 @@ public class npcsight : MonoBehaviour
 
     }
 
-    //npc ���� ���� �þ� ����
+    //NPC가 보는 방향으로 시야 각도 변경
     private Vector2 RotateVector(Vector3 direction, float offsetAngle)
     {
         float angleRadius = offsetAngle * Mathf.Deg2Rad;
